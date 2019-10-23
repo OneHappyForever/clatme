573a74003619e2d7cce433df4369b4a299eb8a2b
cloudfreecn <48370727+cloudfreecn@users.noreply.github.com>
2019-10-23 15:54:14 +0800

    Update srtar.sh

--- srtar.sh
+++ srtar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install git -y && cd /home/www && git clone https://github.com/cloudfreecn/V2hou && cd V2hou && cd
+apt install git -y && cd /home/www && git clone https://github.com/cloudfreecn/V2hou && cd V2hou && cd /root && rm -rf srtar.sh
