d034d326ef93cea80a3f793f06b764eb564f9ae8
cloudfreecn <48370727+cloudfreecn@users.noreply.github.com>
2019-11-12 21:54:40 +0800

    Update srtar.sh

--- srtar.sh
+++ srtar.sh
@@ -4,24 +4,12 @@ timedatectl set-timezone Asia/Shanghai
 
 apt install git -y 
 
-yum install git -y
+git clone https://github.com/cloudfreecn/cf.git
 
-mkdir /home/sshv2
-
-cd /home/sshv2
-
-git clone https://github.com/cloudfreecn/V2hou 
-
-cd V2hou 
+cd cf
 
 chmod +x *
 
 bash go.sh
 
-nohup ./main &
 
-chmod +x *
-
-cd /root
-
-rm -rf srtar.sh
